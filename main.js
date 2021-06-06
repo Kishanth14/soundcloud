6b7b66533aae40223f915d2a61aaf835acc85ccf
Kishanth14 <kishantharulselvan14@gmail.com>
2021-06-06 21:40:32 +0530

    soundcloud

---
+++ main.js
@@ -0,0 +1,211 @@
+SC.initialize({
+    client_id: '340f063c670272fac27cfa67bffcafc4'
+  });
+
+  $(document).ready(function(){   
+      
+      SC.stream('/tracks/25823321',function(sound){ 
+
+          $('#start1').click(function(e) {
+            e.preventDefault();
+            sound.start();
+          });
+
+          $('#stop1').click(function(e){
+              e.preventDefault();
+              sound.stop();
+          });
+ 
+      });
+
+      SC.stream('/tracks/169830775',function(sound){ 
+
+        $('#start2').click(function(e) {
+          e.preventDefault();
+          sound.start();
+        });
+
+        $('#stop2').click(function(e){
+            e.preventDefault();
+            sound.stop();
+        });
+
+    });
+
+    SC.stream('/tracks/302092899',function(sound){ 
+
+      $('#start3').click(function(e) {
+        e.preventDefault();
+        sound.start();
+      });
+
+      $('#stop3').click(function(e){
+          e.preventDefault();
+          sound.stop();
+      });
+
+  });
+  
+  SC.stream('/tracks/571882998',function(sound){ 
+
+    $('#start4').click(function(e) {
+      e.preventDefault();
+      sound.start();
+    });
+
+    $('#stop4').click(function(e){
+        e.preventDefault();
+        sound.stop();
+    });
+
+});
+
+SC.stream('/tracks/378242777',function(sound){ 
+
+  $('#start5').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('#stop5').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/555784899',function(sound){ 
+
+  $('#start6').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('#stop6').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/947119618',function(sound){ 
+
+  $('#start7').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('#stop7').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/822309949',function(sound){ 
+
+  $('#start8').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('#stop8').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/25823321',function(sound){ 
+
+  $('#start9').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('#stop9').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/25823321',function(sound){ 
+
+  $('#start10').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('#stop11').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/25823321',function(sound){ 
+
+  $('#start12').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('#stop12').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/25823321',function(sound){ 
+
+  $('#start13').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('#stop13').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+SC.stream('/tracks/25823321',function(sound){ 
+
+  $('#start14').click(function(e) {
+    e.preventDefault();
+    sound.start();
+  });
+
+  $('#stop14').click(function(e){
+      e.preventDefault();
+      sound.stop();
+  });
+
+});
+
+
+      
+  });   
+
+//  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" 
+//  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/25823321&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/gopiyadav" title="gopiyadav" target="_blank" style="color: #cccccc; text-decoration: none;">gopiyadav</a> · <a href="https://soundcloud.com/gopiyadav/vannam-konda-vennilave-sigaram" title="Vannam Konda Vennilave ( SIGARAM )" target="_blank" style="color: #cccccc; text-decoration: none;">Vannam Konda Vennilave ( SIGARAM )</a></div>
+//  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/169830775&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/tvkappa" title="feel good songs" target="_blank" style="color: #cccccc; text-decoration: none;">feel good songs</a> · <a href="https://soundcloud.com/tvkappa/music-mojo-ilayaraja-medley-by-thaikkudam-bridge-kappa-tv" title="Music Mojo - Ilayaraja Medley By Thaikkudam Bridge - Kappa TV" target="_blank" style="color: #cccccc; text-decoration: none;">Music Mojo - Ilayaraja Medley By Thaikkudam Bridge - Kappa TV</a></div>
+//  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/302092899&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/kailashiyer" title="Kailashiyer" target="_blank" style="color: #cccccc; text-decoration: none;">Kailashiyer</a> · <a href="https://soundcloud.com/kailashiyer/ar-rahman-urvasi-urvasi-unplugged" title="AR Rahman Urvasi Urvasi Unplugged" target="_blank" style="color: #cccccc; text-decoration: none;">AR Rahman Urvasi Urvasi Unplugged</a></div>
+//  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/571882998&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/kabi-8" title="kabi ♫" target="_blank" style="color: #cccccc; text-decoration: none;">kabi ♫</a> · <a href="https://soundcloud.com/kabi-8/viswasam-kannana-kanne" title="Viswasam - Kannana Kanne" target="_blank" style="color: #cccccc; text-decoration: none;">Viswasam - Kannana Kanne</a></div>
+//  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/378242777&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/arrtamilsongs" title="A.R.Rahman Tamil Songs" target="_blank" style="color: #cccccc; text-decoration: none;">A.R.Rahman Tamil Songs</a> · <a href="https://soundcloud.com/arrtamilsongs/raavanan-usure-pogudhey-kaattu-sirukki" title="Raavanan-Usure Pogudhey-Kaattu Sirukki" target="_blank" style="color: #cccccc; text-decoration: none;">Raavanan-Usure Pogudhey-Kaattu Sirukki</a></div>
+//  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/555784899&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/arrtamilsong1" title="A.R.Rahman Tamil Songs" target="_blank" style="color: #cccccc; text-decoration: none;">A.R.Rahman Tamil Songs</a> · <a href="https://soundcloud.com/arrtamilsong1/en-kadhale" title="En Kadhale" target="_blank" style="color: #cccccc; text-decoration: none;">En Kadhale</a></div>
+//  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/947119618&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/djrascalsham" title="DJ Rascal Sham" target="_blank" style="color: #cccccc; text-decoration: none;">DJ Rascal Sham</a> · <a href="https://soundcloud.com/djrascalsham/kaatu-sirikki-remix-raavanan" title="Kaatu Sirikki Remix - Raavanan" target="_blank" style="color: #cccccc; text-decoration: none;">Kaatu Sirikki Remix - Raavanan</a></div>
+//  <iframe width="100%" height="300" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/822309949&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/shijo-joshi-kuzhikkadu" title="Shijo Joshi Kuzhikkadu" target="_blank" style="color: #cccccc; text-decoration: none;">Shijo Joshi Kuzhikkadu</a> · <a href="https://soundcloud.com/shijo-joshi-kuzhikkadu/karabuu-pogaru-dhruva-sarja" title="Karabuu-Pogaru-Dhruva Sarja" target="_blank" style="color: #cccccc; text-decoration: none;">Karabuu-Pogaru-Dhruva Sarja</a></div>
+//  
+
+
+
+
+
+
+
+
+
